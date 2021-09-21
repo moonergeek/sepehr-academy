9c1f4f866faceb07a24028b50718c0742298ab08
Mahan Jafari <mahanjafari14@gmail.com>
2021-09-21 21:17:10 +0430

    Clean Code Proccess

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./container/App";
 import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("sepehr"));
