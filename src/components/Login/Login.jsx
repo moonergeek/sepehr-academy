a3d9afdff033f39ccdcafbdde326a282e75e4a41
Mahan Jafari <mahanjafari14@gmail.com>
2021-09-24 17:52:44 +0330

    Login Page Created

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="container">
+      <div className="d-flex justify-content-center">
+        <h2 className="mt-4">صفحه لاگین...</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
