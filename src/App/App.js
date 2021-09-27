8667bfdb165da8cc6ce56415e10cb368ba51d27c
Mahan Jafari <mahanjafari14@gmail.com>
2021-09-27 16:19:51 +0330

    404 Finished

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,6 @@
 import Course from "../components/Course/Course";
 import NotFound from "../components/NotFound/NotFound";
+import Login from "../components/Login/Login";
 import "./App.css";
 
 const App = () => {
--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -7,22 +7,21 @@ const NotFound = () => {
     <div className="container">
       <div className="row d-flex justify-content-center">
         <img
-          className="mt-5 d-flex justify-content-center"
+          className=" d-flex justify-content-center"
           style={{ width: "600px" }}
           src={NotFound1}
           alt="NotFound"
         />
         <h3
-          className="mt-5 fw-bold d-flex justify-content-center"
+          className="mt-4 fw-bold d-flex justify-content-center"
           style={{ color: "#323E73" }}
         >
           صفحه مورد نظر یافت نشد!
         </h3>
-        <h5
-          className="mt-3 fw-bold d-flex justify-content-center"
-          style={{ color: "#61CE70" }}
-        >
-          <ins className="fw-bold">بازگشت به صفحه قبلی</ins>
+        <h5 className="mt-3 d-flex justify-content-center">
+          <button style={{ backgroundColor: "#61CE78" }} className="btn text-white">
+            بازگشت به صفحه قبلی
+          </button>
         </h5>
       </div>
     </div>
