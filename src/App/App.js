54486047e79bc53144d5af1c0786bbe05232d843
Mahan Jafari <mahanjafari14@gmail.com>
2021-09-29 01:58:47 +0330

    Register So So

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,13 @@
 import Course from "../components/Course/Course";
 import NotFound from "../components/NotFound/NotFound";
 import Login from "../components/Login/Login";
+import Register from "../components/Register/Register";
 import "./App.css";
 
 const App = () => {
   return (
     <>
-      <Course />
+      <Register />
     </>
   );
 };
