a6bfa14ac1b72c8a69331b8dbab4d39923575409
Mahan Jafari <mahanjafari14@gmail.com>
2021-10-12 22:19:19 +0330

    Bug Fixed

--- src/components/Login/BtnContainer/BtnContainer.jsx
+++ src/components/Login/BtnContainer/BtnContainer.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const BtnContainer = () => {
   return (
     <div className="btn-container mb-1 d-inline-flex flex-wrap justify-content-center rounded-pill">
-      <button className={`btn m-1 rounded-pill text-color`}>
-        ثبت نام
-      </button>
+      <Link to="/register">
+        <button className={`btn m-1 rounded-pill text-color`}>ثبت نام</button>
+      </Link>
+
       <button className={`btn m-1 rounded-pill text-color bg-light`}>
         ورود کاربران
       </button>
--- src/components/Register/BtnContainer/BtnContainer.jsx
+++ src/components/Register/BtnContainer/BtnContainer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const BtnContainer = () => {
   return (
@@ -6,9 +7,11 @@ const BtnContainer = () => {
       <button className={`btn m-1 rounded-pill text-color bg-light`}>
         ثبت نام
       </button>
-      <button className={`btn m-1 rounded-pill text-color`}>
-        ورود کاربران
-      </button>
+      <Link to="/login">
+        <button className={`btn m-1 rounded-pill text-color`}>
+          ورود کاربران
+        </button>
+      </Link>
     </div>
   );
 };
