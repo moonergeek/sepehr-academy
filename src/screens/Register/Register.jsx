1e1eb1038a2361810d52f2ce844afff1105aa75a
Masoud Hesami <masoodhesami79@gmail.com>
2021-10-28 14:44:32 +0330

    merged fixed @_@

--- src/screens/Register/Register.jsx
+++ src/screens/Register/Register.jsx
@@ -9,7 +9,7 @@ import "../../components/Register/Register.css";
 const Register = () => {
   return (
     <div className="container register-container">
-      <div className="mt-4 row p-4">
+      <div className="mt-5 row p-4">
         <div className="mt-3 col-lg-12 col-xl-7 order-xl-last d-flex justify-content-center">
           <RegisterImg />
         </div>
