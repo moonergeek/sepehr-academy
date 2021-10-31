27489282cb923cb6b8a69285d95260083d87caac
Sina_Rze <sinarezaei321@gmail.com>
2021-10-31 16:09:52 +0330

    search box 75%

--- src/components/searchBox/serachbox.jsx
+++ src/components/searchBox/serachbox.jsx
@@ -6,17 +6,21 @@ import CloseIcon from '@mui/icons-material/Close';
 
 const SearchBox = (props) => {
 
-    const [blog, setBlog] = useState([]);
+    const [searchData, setSearchData] = useState([]);
 
     const getBlog = () => {
         axios.get('https://academy-visual.herokuapp.com/api/news')
             .then((response) => {
                 console.log(response.data.result);
                 const myBlog = response.data.result;
-                setBlog(myBlog);
+                setSearchData(myBlog);
             });
     }
+
+
+
     useEffect(() => getBlog(), []);
+
 
     const [filteredData, setFilteredData] = useState([]);
     const [wordEntered, setWordEntered] = useState("");
@@ -24,7 +28,7 @@ const SearchBox = (props) => {
     const handleFilter = (event) => {
         const searchWord = event.target.value;
         setWordEntered(searchWord);
-        const newFilter = blog.filter((value) => {
+        const newFilter = searchData.filter((value) => {
             return value.title.toLowerCase().includes(searchWord.toLowerCase());
         });
 
@@ -42,43 +46,22 @@ const SearchBox = (props) => {
 
 
     return (
-        // <div className={"all"}>
-        //     <input className="form-control me-2" type="search"
-        //            placeholder={props.placeHolder}
-        //            aria-label="Search" value={wordEntered}
-        //            onChange={handleFilter}/>
-        //     <div className="searchIcon">
-        //         {filteredData.length === 0 ? (
-        //             <SearchIcon />
-        //         ) : (
-        //             <CloseIcon id="clearBtn" onClick={clearInput} />
-        //         )}
-        //     </div>
-        //     {filteredData.length != 0 && (
-        //         <div className="dataResult">
-        //             {filteredData.slice(0, 5).map((value, key) => {
-        //                 return (
-        //                     <a className="dataItem" href={value.link} target="_blank">
-        //                         <p>{value.title} </p>
-        //                     </a>
-        //                 );
-        //             })}
-        //         </div>
-        //     )}
-        // </div>
-        <div className="search">
-            <div className="searchInputs">
+
+        <div className="search  me-2">
+            {/*{console.log(searchData)}*/}
+            <div className="searchInputs ">
                 <input
+                    className={"inputTag"}
                     type="text"
                     placeholder={props.placeHolder}
                     value={wordEntered}
                     onChange={handleFilter}
                 />
-                <div className="searchIcon">
+                <div className="searchIcon ">
                     {filteredData.length === 0 ? (
-                        <SearchIcon />
+                        <SearchIcon/>
                     ) : (
-                        <CloseIcon id="clearBtn" onClick={clearInput} />
+                        <CloseIcon id="clearBtn" onClick={clearInput}/>
                     )}
                 </div>
             </div>
