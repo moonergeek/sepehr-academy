e2822593641799f91a770a2676fbf5c90afeb7d0
Masoud Hesami <masoodhesami79@gmail.com>
2021-11-11 11:15:10 +0330

    api getCourse seperated added

---
+++ src/Core/services/api/getCourse.api.js
@@ -0,0 +1,13 @@
+import Axios from "axios";
+
+const MainUrl = process.env.REACT_APP_PUBLIC_PATH;
+const GetCourse = async () => {
+    try {
+        const result = await Axios.get(`${MainUrl}api/term/list?pagenumber=1&pagesize=8`)
+        return result.data.result.terms;
+    } catch {
+        return {};
+    }
+};
+// porsidan soal [... other props] ??
+export default GetCourse;
