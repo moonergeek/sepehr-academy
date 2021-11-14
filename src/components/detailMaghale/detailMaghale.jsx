7966d6df649e2223c43c1a9797b9fd418c16e39f
Sina Rezaei <sinarezaei321@gmail.com>
2021-11-14 19:23:36 +0000

    Update detailMaghale.jsx

--- src/components/detailMaghale/detailMaghale.jsx
+++ src/components/detailMaghale/detailMaghale.jsx
@@ -7,7 +7,7 @@ const DetailMaghale = (props) => {
 
     return (
         <>
-<<<<<<< HEAD
+
             <div className="card mb-3 main">
                 <img src={props.newsById.image} className="card-img-top" alt="..."/>
                 <div className="card-body">
@@ -22,33 +22,7 @@ const DetailMaghale = (props) => {
 
                 </div>
             </div>
-=======
-                        <Router>
-                <Switch>
 
-                    {getData.map(value =>
-                        <Route key={value} path={"/blog/maghale/" + value._id}>
-
-                            <div className="card mb-3 main">
-                                <img src={value.image} className="card-img-top" alt="..." />
-                                <div className="card-body">
-                                    <h5 className="card-title">{value.title}</h5>
-                                    <p className="card-text">{value.text}</p>
-                                    <p className="card-text"><small className="text-muted">نوشته شده در تاریخ 1400/07/19</small></p>
-                                </div>
-                            </div>
-
-                        </Route>
-
-
-
-
-                    )}
-                </Switch>
-            </Router>
-
-
->>>>>>> sina
         </>
 
 
