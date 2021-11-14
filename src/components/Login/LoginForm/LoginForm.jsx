40e71f9103afee38fbe0345e9a3fac8a567db7eb
Sina Rezaei <sinarezaei321@gmail.com>
2021-11-14 19:02:06 +0000

    Update LoginForm.jsx

--- src/components/Login/LoginForm/LoginForm.jsx
+++ src/components/Login/LoginForm/LoginForm.jsx
@@ -3,13 +3,11 @@ import { useFormik } from "formik";
 import LoginButton from "../LoginButton/LoginButton";
 import { Link, useHistory } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-<<<<<<< HEAD
+
 import eyeImg from "../../../assets/img/eyeicon.png";
 import "../Login.css";
 import LoginUser from "../../../core/services/api/Login.api";
-=======
-import LoginUser from "../../../core/services/API/Login.api";
->>>>>>> sina
+
 
 const LoginForm = () => {
   const initialValues = {
@@ -77,15 +75,11 @@ const LoginForm = () => {
           </div>
         </div>
 
-<<<<<<< HEAD
+
         <div className="mb-3 pass-form">
           <input
             type={passwordShown ? "password" : "text"}
-=======
-        <div className="mb-3">
-          <input
-            type="password"
->>>>>>> sina
+
             className="form-control"
             placeholder="رمز عبور"
             id="password"
@@ -95,7 +89,7 @@ const LoginForm = () => {
             onBlur={formik.handleBlur}
           />
 
-<<<<<<< HEAD
+
           <div
             className="pass-eye"
             onClick={() => {
@@ -105,8 +99,7 @@ const LoginForm = () => {
             <img src={eyeImg} />
           </div>
 
-=======
->>>>>>> sina
+
           <div className="text-danger mt-1">
             {formik.touched.password && formik.errors.password ? (
               <div>{formik.errors.password}</div>
