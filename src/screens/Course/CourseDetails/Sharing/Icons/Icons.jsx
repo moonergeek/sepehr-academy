a7f982cb5389304edc56043c4d0219354b073239
Mahan Jafari <mahanjafari14@gmail.com>
2021-11-15 10:06:20 +0330

    social media and request

--- src/screens/Course/CourseDetails/Sharing/Icons/Icons.jsx
+++ src/screens/Course/CourseDetails/Sharing/Icons/Icons.jsx
@@ -1,16 +1,24 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import twitter from "../../../../../assets/img/twitter.svg";
 import telegram from "../../../../../assets/img/telegram(1).svg";
 import facebook from "../../../../../assets/img/facebook.svg";
 
 const Icons = () => {
+  const location = useLocation();
   return (
     <>
       <span className="share-icon">
         <img src={twitter} alt="twitter" />
       </span>
       <span className="share-icon">
-        <img src={telegram} alt="telegram" />
+        <img
+          onClick={() => {
+            console.log(location);
+          }}
+          src={telegram}
+          alt="telegram"
+        />
       </span>
       <span className="share-icon">
         <img src={facebook} alt="facebook" />
