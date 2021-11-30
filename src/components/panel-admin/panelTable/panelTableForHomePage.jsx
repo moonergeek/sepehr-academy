ad25a6413f74465d8915d3926b5b78815b8ba709
Sina_Rze <sinarezaei321@gmail.com>
2021-11-30 12:57:07 +0330

    panelTableForHomePage edited

--- src/components/panel-admin/panelTable/panelTableForHomePage.jsx
+++ src/components/panel-admin/panelTable/panelTableForHomePage.jsx
@@ -10,7 +10,7 @@ import Loading from "../../common/loading/loading";
 const PanelTableForHpmePage = (props) => {
 
     const [allTerms, setAllTerms] = useState([]);
-    const [loading , setLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
     const [userInformation, setUserInformation] = useState([]);
 
     const getUserInformation = async () => {
@@ -35,7 +35,7 @@ const PanelTableForHpmePage = (props) => {
 
     return (
         <>
-            {loading ?             <table className="table panel-table table-borderless">
+            {loading ? <table className="table panel-table table-borderless">
                 <thead className={"thead-color"}>
                 <tr className={"tr-color"}>
                     <th scope="col "><FcNumericalSorting12/></th>
@@ -51,31 +51,55 @@ const PanelTableForHpmePage = (props) => {
                 <tbody>
 
                 {Object.keys(allTerms).map(termObj => <tr key={termObj} className={"green-hover"}>
-                    <th scope="row" className={"panel-th-items"}>{termObj}</th>
-                    <td className={"panel-td-items"}>4554-01</td>
-                    <td className={"panel-td-items"}>
+
+                    {userInformation.terms[termObj] ? <>
+                        <th scope="row" className={"panel-th-items"}>{termObj}</th>
+                        <td className={"panel-td-items"}>4554-01</td>
+                        <td className={"panel-td-items"}>
+                            <img className={"table-course-img"} src={userInformation.terms[termObj].course.image}
+                                 alt=""/>
+                            {userInformation.terms[termObj].course.courseName}
+                        </td>
+                        <td className={"panel-td-items"}> {userInformation.terms[termObj].course.description}</td>
+                        <td className={"panel-td-items"}> 1400/01/20</td>
+
+                        <td className={"panel-td-items"}>
+                            {userInformation.terms[termObj] ? <span className="badge bg-success"> خریداری شده</span> :
+                                <span className="badge bg-primary">در حال خرید</span>}
+
+                        </td>
+                        <td className={"panel-td-items"}>{userInformation.terms[termObj].cost + " تومان"} </td>
+                        <td className={"panel-td-items"}>
+
+                            <PanelAddIcon userInfo={props.userInfo} termId={userInformation.terms[termObj]._id}/>
+                        </td>
+                    </> :        <>             <th scope="row" className={"panel-th-items"}>{termObj}</th>
+                        <td className={"panel-td-items"}>4554-01</td>
+                        <td className={"panel-td-items"}>
                         <img className={"table-course-img"} src={allTerms[termObj].course.image} alt=""/>
-                        {allTerms[termObj].course.courseName}
-                    </td>
-                    <td className={"panel-td-items"}> {allTerms[termObj].course.description}</td>
-                    <td className={"panel-td-items"}> 1400/01/20</td>
+                    {allTerms[termObj].course.courseName}
+                        </td>
+                        <td className={"panel-td-items"}> {allTerms[termObj].course.description}</td>
+                        <td className={"panel-td-items"}> 1400/01/20</td>
 
-                    <td className={"panel-td-items"}>
-                        {userInformation.terms[termObj] ? <span className="badge bg-success"> خریداری شده</span> :
-                            <span className="badge bg-primary">در حال خرید</span>}
+                        <td className={"panel-td-items"}>
+                    {userInformation.terms[termObj] ? <span className="badge bg-success"> خریداری شده</span> :
+                        <span className="badge bg-primary">در حال خرید</span>}
 
-                    </td>
-                    <td className={"panel-td-items"}>{allTerms[termObj].cost + " تومان"} </td>
-                    <td className={"panel-td-items"}>
+                        </td>
+                        <td className={"panel-td-items"}>{allTerms[termObj].cost + " تومان"} </td>
+                        <td className={"panel-td-items"}>
 
                         <PanelAddIcon userInfo={props.userInfo} termId={allTerms[termObj]._id}/>
-                    </td>
+                        </td> </>}
+
+
+
                 </tr>)}
 
 
                 </tbody>
-            </table> : <Loading />}
-
+            </table> : <Loading/>}
 
 
         </>
