53b7dc6fb51f94e6fe178dde152c16a9888d50aa
Mahan Jafari <mahanjafari14@gmail.com>
2021-12-03 17:34:37 +0330

    bug fixed

--- src/screens/panel-Admin/panelAdmin.jsx
+++ src/screens/panel-Admin/panelAdmin.jsx
@@ -64,6 +64,7 @@ const PanelAdmin = () => {
                                 onClick={() => {
                                     clearStorage();
                                     history.push("/");
+                                    window.location.reload(true);
                                 }}
                             >
                                 بله
